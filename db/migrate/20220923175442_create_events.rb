0e62722921d07feb9dd4b2fe3628ffbe162fd245
Griffin <griffin@yardzen.com>
2022-09-23 14:29:41 -0400

    Initial commit and DB table

---
+++ db/migrate/20220923175442_create_events.rb
@@ -0,0 +1,27 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.datetime :event_at
+      t.float :indoor_temperature 
+      t.float :indoor_humidity
+      t.float :temperature
+      t.float :feels_like
+      t.float :humidity
+      t.float :wind_direction
+      t.float :wind_direction_ten_minute_average
+      t.float :wind_speed
+      t.float :wind_speed_ten_minute_average
+      t.float :wind_speed_max_daily
+      t.float :wind_speed_gust
+      t.float :event_rain
+      t.float :hourly_rain
+      t.float :weekly_rain
+      t.float :monthly_rain
+      t.float :yearly_rain
+      t.float :uv_index
+      t.datetime :last_rain_at
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,41 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_175442) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "event_at"
+    t.float "indoor_temperature"
+    t.float "indoor_humidity"
+    t.float "temperature"
+    t.float "feels_like"
+    t.float "humidity"
+    t.float "wind_direction"
+    t.float "wind_direction_ten_minute_average"
+    t.float "wind_speed"
+    t.float "wind_speed_ten_minute_average"
+    t.float "wind_speed_max_daily"
+    t.float "wind_speed_gust"
+    t.float "event_rain"
+    t.float "hourly_rain"
+    t.float "weekly_rain"
+    t.float "monthly_rain"
+    t.float "yearly_rain"
+    t.float "uv_index"
+    t.datetime "last_rain_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
