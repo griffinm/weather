5506c1fc08139c1e60d397935c616c46f407efd9
Griffin <griffin@yardzen.com>
2022-09-26 12:42:49 -0400

    Halfs the amount of API poles

--- config/application.rb
+++ config/application.rb
@@ -1,31 +1,31 @@
 require_relative "boot"
 
-require "rails"
+require "rails/all"
 
 # This list is here as documentation only - it's not used
-omitted = %w(
+# omitted = %w(
 
-  )
+#   )
   
-  # Only the frameworks in Rails that do not pollute our routes
-  %w(
-    action_cable/engine
-    action_mailbox/engine
-    action_text/engine
-    sprockets/railtie
-  action_mailer/railtie
-  active_storage/engine
-  active_record/railtie
-  action_controller/railtie
-  action_view/railtie
-  active_job/railtie
-  rails/test_unit/railtie
-).each do |railtie|
-  begin
-    require railtie
-  rescue LoadError
-  end
-end
+#   # Only the frameworks in Rails that do not pollute our routes
+#   %w(
+#   action_cable/engine
+#   action_mailbox/engine
+#   action_text/engine
+#   sprockets/railtie
+#   action_mailer/railtie
+#   active_storage/engine
+#   active_record/railtie
+#   action_controller/railtie
+#   action_view/railtie
+#   active_job/railtie
+#   rails/test_unit/railtie
+# ).each do |railtie|
+#   begin
+#     require railtie
+#   rescue LoadError
+#   end
+# end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
